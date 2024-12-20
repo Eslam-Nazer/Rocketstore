6e6eed7843da39bd3c0a1e73f9498d8e7eb70034
Eslam-Nazer <eslam.nazer43@gmail.com>
2024-12-20 03:34:00 +0200

    Confirm admin login and middleware admin routes

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,11 @@ Route::get('/admin', [AuthController::class, 'adminLoginView'])->name('admin-log
 Route::get('/admin/login', [AuthController::class, 'adminLoginView'])->name('admin-login');
 
 Route::post('/admin', [AuthController::class, 'adminAuth'])->name('admin-auth');
+Route::post('/admin/login', [AuthController::class, 'adminAuth'])->name('admin-auth');
 Route::get('/admin/logout', [AuthController::class, 'adminLogout'])->name('admin-logout');
 
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-})->name('admin-dashboard');
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin-dashboard');
+});
