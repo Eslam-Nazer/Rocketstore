4a55c882424587db34208219305de68bca55a712
Eslam-Nazer <eslam.nazer43@gmail.com>
2025-01-12 12:12:50 +0200

    Ajax function to get sub_category depend on category

--- app/Http/Controllers/Admin/SubCategory/SubCategoryActionsController.php
+++ app/Http/Controllers/Admin/SubCategory/SubCategoryActionsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin\SubCategory;
 
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use App\Jobs\Admin\SubCategory\SubCategoryStoreJob;
 use App\Jobs\Admin\SubCategory\SubCategoryDeleteJob;
@@ -38,10 +40,32 @@ class SubCategoryActionsController extends Controller
             ->with("success", "Sub Category successfully updated");
     }
 
-    public function deleteSubCategory(int $id)
+    /**
+     * Summary of deleteSubCategory
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function deleteSubCategory(int $id): RedirectResponse
     {
         SubCategoryDeleteJob::dispatch($id);
         return redirect()->route('sub_category-list')
             ->with('info', 'Sub Category successfully deleted');
+    }
+
+    /**
+     * Summary of ajaxGetSubCategory
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function ajaxGetSubCategory(Request $request): JsonResponse
+    {
+        $conditions['category_id'] = $request->id;
+        $sub_categories = SubCategory::active($conditions)->get();
+        $html = '';
+        foreach ($sub_categories as $sub_category) {
+            $html .= "<option name='sub_category' value='{$sub_category->id}'>{$sub_category->name}</option>";
+        }
+        $data['html'] = $html;
+        return response()->json($data);
     }
 }
