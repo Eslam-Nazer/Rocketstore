bea7f49c09e47e26121f7cb2a0093e06fefa6c85
Eslam-Nazer <eslam.nazer43@gmail.com>
2025-01-21 19:01:20 +0200

    Change brand operations to right way

--- app/Http/Controllers/Admin/Brand/BrandActionsController.php
+++ app/Http/Controllers/Admin/Brand/BrandActionsController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Admin\Brand;
 
+use App\Models\Brand;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 use App\Jobs\Admin\Brand\BrandStoreJob;
 use App\Jobs\Admin\Brand\BrandDeleteJob;
 use App\Jobs\Admin\Brand\BrandUpdateJob;
@@ -12,23 +16,64 @@ use App\Http\Requests\Admin\Brand\BrandEditingRequest;
 
 class BrandActionsController extends Controller
 {
-    public function insertBrand(BrandInfoRequest $request)
+    /**
+     * Summary of insertBrand
+     * @param \App\Http\Requests\Admin\Brand\BrandInfoRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function insertBrand(BrandInfoRequest $request): RedirectResponse
     {
-        BrandStoreJob::dispatch($request->validated());
+        $validated = $request->validated();
+        extract($validated);
+        $slug = strtolower(trim($slug));
+        Brand::create([
+            'name'              => trim($name),
+            'slug'              => Str::slug($slug),
+            'status'            => $status,
+            'meta_title'        => trim($meta_title),
+            'meta_description'  => trim($meta_description),
+            'meta_keywords'     => trim($meta_keywords),
+            'created_by'        => Auth::user()->id
+        ]);
         return redirect()->route('brand-list')
             ->with('success', 'Brand successfully created');
     }
 
-    public function updateBrand(BrandEditingRequest $request, int $id)
+    /**
+     * Summary of updateBrand
+     * @param \App\Http\Requests\Admin\Brand\BrandEditingRequest $request
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function updateBrand(BrandEditingRequest $request, int $id): RedirectResponse
     {
-        BrandUpdateJob::dispatch($request->validated(), $id);
+        $validated = $request->validated();
+        extract($validated);
+
+        $slug = strtolower(trim($slug));
+        Brand::where('id', '=', $id)
+            ->update([
+                'name'          => trim($name),
+                'slug'          => Str::slug($slug),
+                'status'        => $status,
+                'meta_title'        => trim($meta_title),
+                'meta_description'  => trim($meta_description),
+                'meta_keywords'     => trim($meta_keywords),
+                'created_by'        => Auth::user()->id
+            ]);
         return redirect()->route('brand-list')
             ->with('success', 'Brand Successfully updated');
     }
 
-    public function deleteBrand(int $id)
+    /**
+     * Summary of deleteBrand
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function deleteBrand(int $id): RedirectResponse
     {
-        BrandDeleteJob::dispatch($id);
+        Brand::where('id', '=', $id)
+            ->delete();
         return redirect()->route('brand-list')
             ->with('info', 'Brand successfully deleted');
     }
