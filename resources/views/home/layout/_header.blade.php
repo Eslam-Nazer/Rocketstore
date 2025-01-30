adebcd0c369fe4b40b9dd5667b115d215004fc21
Eslam-Nazer <eslam.nazer43@gmail.com>
2025-01-30 23:20:19 +0200

    Make category and subcategory dynamic in front

--- resources/views/home/layout/_header.blade.php
+++ resources/views/home/layout/_header.blade.php
@@ -59,48 +59,30 @@
                             <a href="{{ url('') }}" class="sf">Home</a>
                         </li>
                         <li>
-                            <a href="category.html" class="sf-with-ul">Shop</a>
+                            <a class="sf-with-ul" style="cursor: pointer;">Shop</a>
 
                             <div class="megamenu megamenu-md">
                                 <div class="row no-gutters">
                                     <div class="col-md-12">
                                         <div class="menu-col">
+
                                             <div class="row">
+                                                @foreach ($activeCategories as $category)
                                                 <div class="col-md-4">
-                                                    <div class="menu-title">Shop with sidebar</div><!-- End .menu-title -->
+                                                    <div class="menu-title"><a href="{{ $category->slug }}">{{ $category->name }}</a></div><!-- End .menu-title -->
                                                     <ul>
-                                                        <li><a href="category-list.html">Shop List</a></li>
-                                                        <li><a href="category-2cols.html">Shop Grid 2 Columns</a></li>
-                                                        <li><a href="category.html">Shop Grid 3 Columns</a></li>
-                                                        <li><a href="category-4cols.html">Shop Grid 4 Columns</a></li>
-                                                        <li><a href="category-market.html"><span>Shop Market<span class="tip tip-new">New</span></span></a></li>
+                                                        @forelse ($category->subCategory as $subCategory)
+                                                        <li><a href="{{$category->slug . '/' . $subCategory->slug}}">{{$subCategory->name}}</a></li>
+                                                        @empty
+                                                        <li><a>{{ "not subcategory found" }}</a></li>
+                                                        @endforelse
                                                     </ul>
+                                                </div><!-- End .col-md-4 -->
+                                                @endforeach
 
-                                                    <div class="menu-title">Shop no sidebar</div><!-- End .menu-title -->
-                                                    <ul>
-                                                        <li><a href="category-boxed.html"><span>Shop Boxed No Sidebar<span class="tip tip-hot">Hot</span></span></a></li>
-                                                        <li><a href="category-fullwidth.html">Shop Fullwidth No Sidebar</a></li>
-                                                    </ul>
-                                                </div><!-- End .col-md-6 -->
-
-                                                <div class="col-md-4">
-                                                    <div class="menu-title">Product Category</div><!-- End .menu-title -->
-                                                    <ul>
-                                                        <li><a href="product-category-boxed.html">Product Category Boxed</a></li>
-                                                        <li><a href="product-category-fullwidth.html"><span>Product Category Fullwidth<span class="tip tip-new">New</span></span></a></li>
-                                                    </ul>
-                                                    <div class="menu-title">Shop Pages</div><!-- End .menu-title -->
-                                                    <ul>
-                                                        <li><a href="cart.html">Cart</a></li>
-                                                        <li><a href="checkout.html">Checkout</a></li>
-                                                        <li><a href="wishlist.html">Wishlist</a></li>
-                                                        <li><a href="dashboard.html">My Account</a></li>
-                                                        <li><a href="#">Lookbook</a></li>
-                                                    </ul>
-                                                </div><!-- End .col-md-6 -->
                                             </div><!-- End .row -->
                                         </div><!-- End .menu-col -->
-                                    </div><!-- End .col-md-8 -->
+                                    </div><!-- End .col-md-12 -->
 
                                 </div><!-- End .row -->
                             </div><!-- End .megamenu megamenu-md -->
